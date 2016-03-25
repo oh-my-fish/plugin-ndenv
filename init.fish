40c659282bb664fba4201615817078c94bdf64d8
Justin Hileman <justin@justinhileman.info>
2016-03-25 00:09:12 -0700

    Only add missing paths during init.
    This prevents issues when reloading the shell. See oh-my-fish/plugin-composer#7

--- init.fish
+++ init.fish
@@ -1,3 +1,9 @@
 set -q NDENV_ROOT; or set -l NDENV_ROOT $HOME/.ndenv
 
-set PATH $NDENV_ROOT/shims $NDENV_ROOT/bin $PATH
+if not contains $NDENV_ROOT/bin $PATH
+  set PATH $NDENV_ROOT/bin $PATH
+end
+
+if not contains $NDENV_ROOT/shims $PATH
+  set PATH $NDENV_ROOT/shims $PATH
+end
